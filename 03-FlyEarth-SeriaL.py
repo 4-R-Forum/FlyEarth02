618e77003e97b916ac57e2c7dadd9c2541059f61
4-R-Forum <jhodge@4-R-Forum.org>
2023-10-07 15:00:59 -0400

    07-Fly.py updated

--- 03-FlyEarth-SeriaL.py
+++ 03-FlyEarth-SeriaL.py
@@ -1,21 +1,25 @@
 import sys
+import time
 import serial
-# $port = new-Object System.IO.Ports.SerialPort  COM3,115200,None,8,one
 global port
 global cmd_string
 try:
   #port = serial.Serial('COM3',115200,None,'EIGHTBITS','STOPBITS_ONE')
   port = serial.Serial(port = "COM3", baudrate=115200,bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-
-
 except:
   sys.stdout.write("Cannot open Port COM3")
   sys.exit()
 while True :
-  try: 
+  try:
+    # request command
+    #port.write(17) # XON
+    req = "|".encode("utf-8")
+    port.writelines(bytes(req))
     # get command 
     cmd_string = port.readline().decode("utf-8") 
     sys.stdout.write(cmd_string)
+    #port.write(19) # XOFF
+
   except  Exception as e:
     sys.stdout.writelines(str(e))
     break
