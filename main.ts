3bc83a73dbe963d82810d9092bc3c651a44c9f26
4-R-Forum <jhodge@4-R-Forum.org>
2023-10-11 18:21:57 -0400

    merged changes from FlyEarth03, use 02 only

--- main.ts
+++ main.ts
@@ -1,20 +1,29 @@
-let cmdString = ""
+/**
+ * Signed in with Google
+ */
+let cmd = ""
 let roll = 0
 let pitch = 0
-let updown = 0
+let button = 0
 serial.redirectToUSB()
+let count = 0
+basic.showIcon(IconNames.SmallDiamond)
 basic.forever(function () {
-    if (serial.readLine() == "req") {
-        if (input.buttonIsPressed(Button.A)) {
-            updown = 1
-        } else {
-            if (input.buttonIsPressed(Button.B)) {
-                updown = -1
-            }
-        }
-        pitch = input.rotation(Rotation.Pitch)
-        roll = input.rotation(Rotation.Roll)
-        cmdString = "" + convertToText(updown) + "," + convertToText(pitch) + "," + convertToText(roll)
-        serial.writeLine(cmdString)
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showIcon(IconNames.Square)
+        serial.writeLine("-1,0,0,0")
+        game.pause()
     }
+    count = count + 1
+    if (input.buttonIsPressed(Button.A)) {
+        button = 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        button = -1
+    }
+    pitch = input.rotation(Rotation.Pitch)
+    roll = input.rotation(Rotation.Roll)
+    cmd = "" + convertToText(count) + " , " + convertToText(pitch) + " , " + convertToText(roll) + ", " + convertToText(button)
+    serial.writeLine(cmd)
+    basic.pause(1000)
 })
